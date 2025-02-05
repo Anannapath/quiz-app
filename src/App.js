6bed7e5778186136290abdfb6b8826b8eaf911c1
Anannapath <152575328+Anannapath@users.noreply.github.com>
2025-02-05 15:46:08 +0700

    Quiz Application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import {createContext, useState} from "react";
+import Menu from "./component/Menu";
+import Score from "./component/Score";
+import Quiz from "./component/Quiz";
+import './App.css'
+
+export const DataContext = createContext()
 
 function App() {
+
+  const [appState, setAppState] = useState("menu")
+  const [score,setScore] = useState(0)
+
   return (
-    <div className="App">
-      <h1>Web Development Quiz</h1>
-    </div>
+
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+      <div className="App">
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu/>} 
+        {appState === "quiz" && <Quiz/>}
+        {appState === "score" && <Score/>}
+      </div>
+    </DataContext.Provider>
+    
   );
 }
 
